21194e01fa08b12593d5705c3f57190e9a071b04
GigaGyl <gilchristweru@gmail.com>
2025-10-10 08:59:47 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App()
+{
+  return (
+    <div>
+      <div>
+        <header className='absolute top-0 text-xl p-5 bg-gray-600 w-full text-center rounded'>
+          Tugas yh
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
